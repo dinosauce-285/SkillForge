999d767836ab7cecb4fe532a0ce8a92e6c509ba8
dinosauce-285 <thanhbualqt@gmail.com>
2026-04-04 10:08:37 +0700

    fix(client): restore startup flow and emulator host

--- client/app/src/main/java/com/example/skillforge/core/di/AppContainer.kt
+++ client/app/src/main/java/com/example/skillforge/core/di/AppContainer.kt
@@ -35,7 +35,7 @@ class AppContainer {
     }
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://192.168.1.7:3000/")
+        .baseUrl("http://10.0.2.2:3000/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
