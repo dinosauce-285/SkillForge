eeb8acf325a4ce7492c7474e8f292002b0e26e01
Trần Quốc Thiện <149931497+tqthienda1@users.noreply.github.com>
2026-04-08 19:36:23 +0700

    Delete path/to/TransactionViewModel.kt

--- path/to/TransactionViewModel.kt
+++
@@ -1,6 +0,0 @@
-data class TransactionUiState(
-    val course: Course, // Added course property
-    val basePrice: Double, // Removed duplicate basePrice property
-    val isLoading: Boolean = false, // Added isLoading field
-    val isSubmitting: Boolean = false // Added isSubmitting field
-)
