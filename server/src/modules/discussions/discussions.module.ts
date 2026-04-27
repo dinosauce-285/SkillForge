8f9a0e06baea168ad602dc93da17fc710744a332
Ngô Bảo Long <ngobaolong0401@gmail.com>
2026-04-28 01:42:29 +0700

    refactor(server): update discussions, progress, and users modules

--- server/src/modules/discussions/discussions.module.ts
+++ server/src/modules/discussions/discussions.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DiscussionsController } from './discussions.controller';
 import { DiscussionsService } from './discussions.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, NotificationsModule],
   controllers: [DiscussionsController],
   providers: [DiscussionsService],
   exports: [DiscussionsService],
